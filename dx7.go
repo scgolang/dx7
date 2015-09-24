ca947a82b6afaa2985474355976adbae02b9d276
Brian Sorahan <bsorahan@gmail.com>
2015-09-23 20:11:57 -0500

    removed some cruft

--- dx7.go
+++ dx7.go
@@ -14,59 +14,58 @@ const (
 	numVoices = 127
 
 	// defName is the name of the synthdef
-	defName = "dx7voice"
+	defaultDefName = "defaultDX7voice"
 )
 
-var (
-	// defaultAlgorithm
-	//
-	//    Op2
-	//     |
-	//    Op1
-	//
-	defaultAlgorithm = sc.NewSynthdef(defName, func(p sc.Params) sc.Ugen {
-		var (
-			gate = p.Add("gate", 1)
+// defaultAlgorithm
+//
+//    Op2
+//     |
+//    Op1
+//
+var defaultAlgorithm = sc.NewSynthdef(defaultDefName, func(p sc.Params) sc.Ugen {
+	var (
+		gate = p.Add("gate", 1)
 
-			op1freq = p.Add("op1freq", 440)
-			op1gain = p.Add("op1gain", 1)
-			op1amt  = p.Add("op1amt", 0)
+		op1freq = p.Add("op1freq", 440)
+		op1gain = p.Add("op1gain", 1)
+		op1amt  = p.Add("op1amt", 0)
 
-			op2freq = p.Add("op2freq", 440)
-			op2gain = p.Add("op2gain", 1)
-			op2amt  = p.Add("op2amt", 0)
+		op2freq = p.Add("op2freq", 440)
+		op2gain = p.Add("op2gain", 1)
+		op2amt  = p.Add("op2amt", 0)
 
-			bus = sc.C(0)
-		)
+		bus = sc.C(0)
+	)
 
-		// modulator
-		op2 := Operator{
-			Freq: op2freq,
-			Amt:  op2amt,
-			Gate: gate,
-			Gain: op2gain,
-			Done: sc.FreeEnclosing,
-		}.Rate(sc.AR)
+	// modulator
+	op2 := Operator{
+		Freq: op2freq,
+		Amt:  op2amt,
+		Gate: gate,
+		Gain: op2gain,
+		Done: sc.FreeEnclosing,
+	}.Rate(sc.AR)
 
-		// carrier
-		op1 := Operator{
-			Freq: op1freq,
-			FM:   op2,
-			Amt:  op1amt,
-			Gate: gate,
-			Gain: op1gain,
-			Done: sc.FreeEnclosing,
-		}.Rate(sc.AR)
+	// carrier
+	op1 := Operator{
+		Freq: op1freq,
+		FM:   op2,
+		Amt:  op1amt,
+		Gate: gate,
+		Gain: op1gain,
+		Done: sc.FreeEnclosing,
+	}.Rate(sc.AR)
 
-		return sc.Out{bus, op1}.Rate(sc.AR)
-	})
-)
+	return sc.Out{bus, op1}.Rate(sc.AR)
+})
 
 // DX7 encapsulates the synth architecture of the legendary Yamaha DX7.
 type DX7 struct {
-	client *sc.Client
-	group  *sc.Group
-	voices [numVoices]*sc.Synth
+	client   *sc.Client
+	group    *sc.Group
+	voices   [numVoices]*sc.Synth
+	curVoice string
 }
 
 // Play plays a note. This can either turn a voice on or
@@ -92,7 +91,7 @@ func (dx7 *DX7) Play(note Note) error {
 		"op1gain": float32(note.Velocity) / (127 * polyphony),
 		"op1amt":  float32(100),
 	}
-	synth, err := dx7.group.Synth(defName, sid, sc.AddToTail, controls)
+	synth, err := dx7.group.Synth(dx7.curVoice, sid, sc.AddToTail, controls)
 	if err != nil {
 		return err
 	}
@@ -109,17 +108,13 @@ func (dx7 *DX7) Control(cc CC) error {
 // NewDX7 returns a DX7 using the defaultAlgorithm.
 // client will be used to create synth nodes, and all the synth
 // nodes will be added to the provided group.
-func NewDX7(client *sc.Client, group *sc.Group) (MIDIHandler, error) {
-	if err := client.SendDef(defaultAlgorithm); err != nil {
+func NewDX7(c *sc.Client, g *sc.Group) (MIDIHandler, error) {
+	if err := c.SendDef(defaultAlgorithm); err != nil {
 		return nil, err
 	}
-	return &DX7{client: client, group: group}, nil
-}
-
-// Wire represents a connection between two operators.
-type Wire struct {
-	// Input is the input side of the wire.
-	Input int `xml:"input"`
-	// Output is the output side of the wire.
-	Output int `xml:"output"`
+	return &DX7{
+		client:   c,
+		group:    g,
+		curVoice: defaultDefName,
+	}, nil
 }
--- main.go
+++ main.go
@@ -1,4 +1,12 @@
-// dx7 is a SuperCollider-based FM synthesizer.
+// `dx7` is an FM synthesizer built with Go and SuperCollider
+// that is inspired by the legendary Yamaha DX7.
+// The synth architecture is built on the same principles as the DX7,
+// but is much more flexible and extensible.
+// Operators consist of a sine wave and envelope generator, and
+// are combined into different algorithms to modulate one another.
+// Instead of being limited to 6 operators and 32 algorithms though,
+// `dx7` allows you to use any number of operators and combine them
+// into algorithms of your own design.
 package main
 
 import (
